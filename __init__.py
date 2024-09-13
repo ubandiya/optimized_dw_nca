94de1042cb9f2527f1fa65769019b603bbe225e5
Ubandiya Najib Yusuf <32720837+ubandiya@users.noreply.github.com>
2024-09-13 10:32:17 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,5 +1,5 @@
 # optimized_dw_nca/__init__.py
 
-from .distance_weighted_nca import DistanceWeightedNCA, DWNCA_KNNClassifier
+from .distance_weighted_nca import DistanceWeightedNCA
 from .metrics import PerformanceMetrics
-from .knn_classifier import KNNClassifier
+from .knn_classifier import DWNCA_KNNClassifier
